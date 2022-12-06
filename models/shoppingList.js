c3e2b13fe194a84fc4f0a2609ff035ac0ab753f8
BushraFatimaBF <vglampard@protonmail.com>
2022-12-06 15:20:52 +0000

    console logs added

--- models/shoppingList.js
+++ models/shoppingList.js
@@ -22,6 +22,7 @@ export async function postListItem(listItem) {
 // create a patch function that will toggle just the completed value
 // receives an object: { id: idOfTickedItem, completed: !item.completed (true/false) }
 export async function patchListItem(id, isCompleted){
+  console.log("patchListItem called")
 const data = await pool.query(
   `UPDATE shopping SET completed = ($1) WHERE id = ($2) RETURNING *;`,
   [isCompleted, id]
